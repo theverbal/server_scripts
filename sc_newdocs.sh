1472818e0937e510909e7b00aa864cf8e20c44e8
verbal <verbal@theverbal.net>
2023-09-10 08:12:04 -0400

    changed xlsx converter to unoconv and tab delimited file.

--- sc_newdocs.sh
+++ sc_newdocs.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
-eml=/home/verbal/Nextcloud/shittycinema/RAW/EpisodeMasterList.csv
-sdir=/home/verbal/Nextcloud/shittycinema/Season6
-oldpn=$(ls -t $sdir | grep PN | head -n1)
-oldplot=$(ls -t $sdir | grep Plot | head -n1)
 
+# this script copies the episode master list to csv
+# then if a recording date is four days away or less
+# create copies of the plot and production notes
+# for each film within the time span
 
-while IFS="," read -r episode film month badperson description releasedate recordeddate editr guest notes
+path=/home/verbal/Nextcloud/shittycinema
+eml=${path}/EpisodeMasterList.xlsx
+emlc=${path}/EpisodeMasterList.csv
+sdir=${path}/Season6
+pn=${path}/zPN.docx
+plot=${path}/zPlot.docx
+curdate=$(date +%m/%d/%Y)
+
+#convert eml to tab delimited file 9(ASCII tab),0(no quotes around strings; 34 double or 39 single)
+unoconv -f csv -e FilterOptions="9,0,9,2" $eml 
+
+while IFS=$'\t' read -r episode film month badperson description releasedate recordeddate editr guest notes
 do
-	[[ $(date +%d%m%Y -d "+8 days") > $(date +%d%m%Y -d "$recordeddate") ]] && cp ${sdir}\$(ls -t $sdir | grep PN | head -n1) ${sdir}\${film}
+	#[[ $(date +%m/%d/%Y -d "+4 days") > $(date +%m/%d/%Y -d "$recordeddate") ]] && cp $pn ${sdir}/${film}" PN.docx" ; cp $plot ${sdir}/${film}" Plot.docx" ; list+=("$film")
+	#[[ $(date +%m/%d/%Y -d "+4 days") > $(date +%m/%d/%Y -d "$recordeddate") && $recordeddate > $curdate ]] && [[ ! -f ${sdir}/${film}" PN.docx" ]] && cp $pn ${sdir}/${film}" PN.docx" && [[ ! -f ${sdir}/${film}" Plot.docx" ]] && cp $plot ${sdir}/${film}" Plot.docx" && list+=("$film")
+	[[ $(date +%m/%d/%Y -d "+4 days") > $(date +%m/%d/%Y -d "$recordeddate") && $(date +%m/%d/%Y -d "$recordeddate")   ]] && echo "$film"
+done < <(tail -n +2 $emlc)
 
-done < <(tail -n +2 $eml)
+rm $emlc
+
+[[ ${#list[@]} -ne 0 ]] && for i in ${list[@]}
+do
+	echo "Created production notes and plot documents for $i"
+done
